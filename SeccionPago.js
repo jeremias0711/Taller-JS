e5dbb496c35fef1af150bf73adf7c6f8a28e745b
jeremias0711 <jgutierrezmo@unbosque.edu.co>
2026-04-09 13:11:13 +0000

    Variables paso 3

--- SeccionPago.js
+++ SeccionPago.js
@@ -2,3 +2,7 @@ let nombreCompleto = "";
 let edad = 0;
 let tipoDocumento = "";
 let numeroDocumento = "";
+let salario = 0;
+let comisiones = 0;
+let totalHorasExtra = 0;
+let calificacionNiveldeRiesgo = "";
