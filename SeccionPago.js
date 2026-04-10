193dcd9d4a9bfea5def1b71f5ca80ef3abca5b35
jeremias0711 <jgutierrezmo@unbosque.edu.co>
2026-04-10 07:56:55 -0500

    Add constants and calculations for IBC, health, and pension
    Added constants for calculations and updated IBC, health, and pension calculations.

--- SeccionPago.js
+++ SeccionPago.js
@@ -8,6 +8,9 @@ let totalHorasExtra = 0;
 let calificacionNiveldeRiesgo = "";
 let valorHorasExtra = 0;
 let niveldeRiesgo = 0;
+
+// Consntantes
+
 const smmlv = 1750905;
 const auxilioTransporte = 250000;
 const porcentajeSalud = 0.04;
@@ -18,3 +21,9 @@ const riesgo2 = 0.01044;
 const riesgo3 = 0.02436;
 const riesgo4 = 0.04350;
 const riesgo5 = 0.06960;
+
+// Calculos
+
+let ibc = (salario+comisiones+totalHorasExtra) * 0.7
+let salud = ibc * porcentajeSalud
+let pension = ibc * porcentajePension
